28cac8ea3260b260ea8c7d48ff77bf2a3397cf33
hamzachaouch00 <124165960+hamzachaouch00@users.noreply.github.com>
2023-06-02 09:40:02 +0100

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const connectDb=async()=>{
     
 try {
-    await mongoose.connect("mongodb+srv://imenkhelil88:imen123@cluster0.bpqz63o.mongodb.net/")
+    await mongoose.connect("mongodb+srv://hamzachauch10:hamza123@cluster0.0dzkd9i.mongodb.net/")
     console.log("db is connected")
 } catch (error) {
     console.log("db is not connected")
